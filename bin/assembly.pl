cafa15a88343edb8c2a62cd1dfb20cc65bac58f0
Paul Cantalupo <pcantalupo@gmail.com>
2017-03-21 17:48:06 -0400

    adding directory option for cwl

--- bin/assembly.pl
+++ bin/assembly.pl
@@ -1,12 +1,20 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
+use Getopt::Long;
+use Cwd;
 
-my $fasta = shift;
+my $fasta;
+my $dir = getcwd;
+GetOptions ("fasta|f=s" => \$fasta,
+            "directory|d=s" => \$dir,
+            );
 
-die "Usage: $0 FASTAFILE" if (!$fasta);
-my $command = "ABYSS -k 25 -o $fasta.contigs $fasta > $fasta.abyss.out 2>&1";
+die "Usage: $0 -f FASTAFILE" if (!$fasta);
 
+my $outputfile = "$dir/$fasta.contigs";
+my $logfile    = "$dir/$fasta.abyss.out";
+my $command = "ABYSS -k 25 -o $outputfile $fasta > $logfile 2>&1";
 print "Running command: $command\n";
 `time $command`;
 
